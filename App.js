f0bd1a25ccb329d6dfcf551a0d78d80458e41cde
Caleb <savershot685@gmail.com>
2018-06-20 12:30:57 -0500

    added my own custom text to the intro project
    I am relived to see that this did not bring up a red screen on my phone! :) I susspect i added an unnecisary period to line 4 in the App.js file.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HelloComponent from './app/components/HelloComponent/HelloComponent';
+
 export default class App extends React.Component {
   render() {
     return (
@@ -8,6 +10,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <HelloComponent />
       </View>
     );
   }
--- app/components/HelloComponent/HelloComponent.js
+++ app/components/HelloComponent/HelloComponent.js
@@ -1,1 +1,15 @@
-import React, {Component} from'react';
+import React, {Component} from 'react';
+import {AppRegistry,Text,View} from 'react-native';
+
+export default class HelloComponent extends Component{
+    render(){
+        return (
+        <View>
+        <Text>Hello World!</Text>
+            <Text> MegaMan 3 is the BEST MegaMan Game EVER on the NES!</Text>
+        </View>
+        )
+    }
+}
+
+AppRegistry.registerComponent('HelloComponent', ()=> HelloComponent);
