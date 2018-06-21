268179baf357abf82632e5721b36617ee699cf0f
Caleb <savershot685@gmail.com>
2018-06-21 10:14:29 -0500

    tampering with text.
    Oh this is cool! I know its do diffrent than what I have been doing, but seeing the results on my iPhone. Its like MAGIC!

--- App.js
+++ App.js
@@ -7,8 +7,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text style={styles.intro}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.intro}>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <HelloComponent />
       </View>
@@ -19,8 +19,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(0,0,156)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+    intro:{
+    textAlign: 'center',
+    color: 'rgba(42,82,190,2)'
+    
+    },
 });
--- app/components/HelloComponent/HelloComponent.js
+++ app/components/HelloComponent/HelloComponent.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
-import {AppRegistry,Text,View} from 'react-native';
+import {StyleSheet, AppRegistry,Text,View} from 'react-native';
 
 export default class HelloComponent extends Component{
     render(){
         return (
         <View>
-        <Text>Hello World!</Text>
-            <Text> MegaMan 3 is the BEST MegaMan Game EVER on the NES!</Text>
+            <Text style={styles.intro}>Hello World!</Text>
+            <Text style={styles.intro}> MegaMan 3 is the BEST MegaMan Game EVER on the NES!</Text>
         </View>
         )
     }
 }
 
 AppRegistry.registerComponent('HelloComponent', ()=> HelloComponent);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    intro:{
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#cc00cc'
+    
+    },
+});
